bdcf65f903ae69f2a3d205c95951ed20505690fa
Dan Winter <dan.j.winter@gmail.com>
2016-03-29 17:40:54 -0600

    jump test pass

--- test/dino-test.js
+++ test/dino-test.js
@@ -5,7 +5,7 @@ var Dino = require('../lib/dino');
 
 describe("Dino", function(){
   it("starts with a default position", function(){
-    var dino = new Dino;
+    var dino = new Dino();
     var defaultX = 50;
     var defaultY = 300;
     var defaultWidth = 10;
@@ -17,16 +17,75 @@ describe("Dino", function(){
     assert.equal(dino.jumpAvailable, true);
     assert.equal(dino.isJumping, false);
     assert.equal(dino.isFalling, false);
-    assert.equal(dino.fallSpeed, 2);
   });
 
-  // it("can't jump when already jumping")
 
   it("can jump", function(){
-    var dino = new Dino;
+    var dino = new Dino();
     dino.jump();
     assert.equal(dino.isJumping, true);
     assert.equal(dino.jumpSpeed, 15);
-    dino.checkJump();
+    assert.equal(dino.jumpAvailable, false);
   });
+
+  it("continues to ascend from jump until speed its 0", function(){
+    var dino = new Dino;
+    var originalY = dino.y;
+    dino.jump();
+    var originalJumpSpeed = dino.jumpSpeed;
+    dino.checkJump();
+    assert.notEqual(dino.y, originalY);
+    assert.equal(dino.y, originalY - dino.jumpSpeed - 1);
+    assert.equal(dino.jumpSpeed, originalJumpSpeed - 1);
+    do {
+      dino.checkJump();
+    } while (dino.jumpSpeed !== 0);
+    assert.equal(dino.isJumping, false);
+    assert.equal(dino.isFalling, true);
+    assert.equal(dino.fallSpeed, 1);
+  })
+
+  it("falls back to original position after jumping", function(){
+    var dino = new Dino();
+    dino.jump();
+    do {
+      dino.checkJump();
+    } while (dino.jumpSpeed !== 0);
+    var crestY = dino.y;
+    dino.checkFall();
+
+    assert.equal(dino.y, crestY + dino.fallSpeed - .8);
+    assert.equal(dino.fallSpeed, 1.8);
+
+    do {
+      dino.checkFall();
+    } while (dino.fallSpeed > 0);
+
+    assert.equal(dino.y, 300);
+    assert.equal(dino.isFalling, false);
+    assert.equal(dino.fallSpeed, 0);
+    assert.equal(dino.jumpAvailable, true)
+  })
+
+  it("stops falling at original y coordinates", function(){
+    var dino = new Dino();
+    dino.jump();
+    dino.stopFall();
+
+    assert.equal(dino.isFalling, false);
+    assert.equal(dino.fallSpeed, 0);
+    assert.equal(dino.jumpAvailable, true);
+  });
+
+  it("can't jump when already jumping", function(){
+    var dino = new Dino();
+    dino.jump();
+    var originalJumpSpeed = dino.jumpSpeed;
+    assert.equal(dino.jumpAvailable, false);
+    assert.equal(originalJumpSpeed, 15);
+    dino.checkJump();
+    assert.equal(dino.jumpSpeed, 14);
+    dino.jump();
+    assert.notEqual(dino.jumpSpeed, 15)
+  })
 });
