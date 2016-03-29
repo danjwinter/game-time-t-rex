8dc3b3668b7afec68bdbec944e640286e81651a3
Dan Winter <dan.j.winter@gmail.com>
2016-03-29 16:13:51 -0600

    write jump test

--- test/dino-test.js
+++ test/dino-test.js
@@ -5,7 +5,7 @@ var Dino = require('../lib/dino');
 
 describe("Dino", function(){
   it("starts with a default position", function(){
-    var dino = new Dino();
+    var dino = new Dino;
     var defaultX = 50;
     var defaultY = 300;
     var defaultWidth = 10;
@@ -14,12 +14,19 @@ describe("Dino", function(){
     assert.equal(dino.y, defaultY);
     assert.equal(dino.width, defaultWidth);
     assert.equal(dino.height, defaultHeight);
+    assert.equal(dino.jumpAvailable, true);
+    assert.equal(dino.isJumping, false);
+    assert.equal(dino.isFalling, false);
+    assert.equal(dino.fallSpeed, 2);
   });
 
+  // it("can't jump when already jumping")
+
   it("can jump", function(){
-    // var dino = new Dino();
-    // dino.jump();
-    // var jumpXPosition = 200;
-    // assert.equal(dino.y, jumpXPosition);
+    var dino = new Dino;
+    dino.jump();
+    assert.equal(dino.isJumping, true);
+    assert.equal(dino.jumpSpeed, 15);
+    dino.checkJump();
   });
 });
