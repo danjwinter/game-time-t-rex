ef16c061ffd82347cb3730ade24413aac27db586
Dan Winter <dan.j.winter@gmail.com>
2016-04-06 18:40:11 -0600

    refactor session

--- test/index.js
+++ test/index.js
@@ -1,8 +1,6 @@
 require('./dino-test');
 require('./cactus-test');
 require('./block-test');
-// require('./create-point-test');
-// require('./points-equal-test');
 require('./bullet-test');
 require('./collision-test');
 require('./score-test');
